051c962cb53cfd54fd95a5f566a27c9f42546eaa
Harsh Kumar <harsh.kumar@pantelwar.com>
2023-09-26 12:25:52 +0530

    change set state to value notifier

--- lib/calculator_screen.dart
+++ lib/calculator_screen.dart
@@ -1,3 +1,5 @@
+import 'dart:math';
+
 import 'package:calculator_app/custom_buttons.dart';
 import 'package:flutter/material.dart';
 import 'package:math_expressions/math_expressions.dart';
@@ -10,66 +12,69 @@ class CalculatorScreen extends StatefulWidget {
 }
 
 class _CalculatorScreenState extends State<CalculatorScreen> {
-  String equation = "0";
-  String result = "0";
+  ValueNotifier<String> equationNotifier = ValueNotifier("0");
+  ValueNotifier<String> resultNotifier = ValueNotifier("0");
   String expression = "";
 
-  buttonPressed(String buttonText) {
-    // decimal k check
-    String doesContainDecimal(dynamic result) {
-      if (result.toString().contains('.')) {
-        List<String> splitDecimal = result.toString().split('.');
-        if (!(int.parse(splitDecimal[1]) > 0)) {
-          return result = splitDecimal[0].toString();
-        }
+  String doesContainDecimal(String result) {
+    if (result.toString().contains('.')) {
+      List<String> splitDecimal = result.toString().split('.');
+      if (!(int.parse(splitDecimal[1]) > 0)) {
+        return result = splitDecimal[0].toString();
       }
-      return result;
     }
+    return result;
+  }
 
-    setState(() {
-      if (buttonText == "AC") {
+  void buttonPressed(String buttonText) {
+    // decimal k check
+    String equation = equationNotifier.value;
+    String result = resultNotifier.value;
+
+    if (buttonText == "AC") {
+      equation = "0";
+      result = "0";
+    } else if (buttonText == "⌫") {
+      equation = equation.substring(0, equation.length - 1);
+      if (equation == "") {
         equation = "0";
-        result = "0";
-      } else if (buttonText == "⌫") {
-        equation = equation.substring(0, equation.length - 1);
-        if (equation == "") {
-          equation = "0";
-        }
-      } else if (buttonText == "+/-") {
-        if (equation[0] != '-' && equation[0] != "0") {
-          equation = '-$equation';
-        } else {
-          equation = equation.substring(1);
-        }
-      } else if (buttonText == "=") {
-        expression = equation;
-        expression = expression.replaceAll('×', '*');
-        expression = expression.replaceAll('÷', '/');
-        expression = expression.replaceAll('%', '%');
-
-        try {
-          Parser p = Parser();
-          Expression exp = p.parse(expression);
-
-          ContextModel cm = ContextModel();
-          result = '${exp.evaluate(EvaluationType.REAL, cm)}';
-          if (expression.contains('%')) {
-            result = doesContainDecimal(result);
-          }
-        } catch (e) {
-          result = "Error";
-        }
-      } else if (equation == "0" &&
-          (buttonText == "×" || buttonText == "÷" || buttonText == "+")) {
-        equation = "0";
-      } else {
-        if (equation == "0") {
-          equation = buttonText;
-        } else {
-          equation = equation + buttonText;
-        }
       }
-    });
+    } else if (buttonText == "+/-") {
+      if (equation[0] != '-' && equation[0] != "0") {
+        equation = '-$equation';
+      } else {
+        equation = equation.substring(1);
+      }
+    } else if (buttonText == "=") {
+      expression = equation;
+      expression = expression.replaceAll('×', '*');
+      expression = expression.replaceAll('÷', '/');
+      expression = expression.replaceAll('%', '%');
+
+      try {
+        Parser p = Parser();
+        Expression exp = p.parse(expression);
+
+        ContextModel cm = ContextModel();
+        result = '${exp.evaluate(EvaluationType.REAL, cm)}';
+        if (expression.contains('%')) {
+          result = doesContainDecimal(result);
+        }
+      } catch (e) {
+        result = "Error";
+      }
+    } else if (equation == "0" &&
+        (buttonText == "×" || buttonText == "÷" || buttonText == "+")) {
+      equation = "0";
+    } else {
+      if (equation == "0") {
+        equation = buttonText;
+      } else {
+        equation = equation + buttonText;
+      }
+    }
+    equationNotifier.value = equation;
+    resultNotifier.value = result;
   }
 
   @override
@@ -104,10 +109,14 @@ class _CalculatorScreenState extends State<CalculatorScreen> {
                         children: <Widget>[
                           Padding(
                               padding: const EdgeInsets.all(10.0),
-                              child: Text(result,
-                                  textAlign: TextAlign.left,
-                                  style: const TextStyle(
-                                      color: Colors.white, fontSize: 80))),
+                              child: ValueListenableBuilder(
+                                  valueListenable: resultNotifier,
+                                  builder: (_, result, ___) {
+                                    return Text(result,
+                                        textAlign: TextAlign.left,
+                                        style: const TextStyle(
+                                            color: Colors.white, fontSize: 80));
+                                  })),
                           const Icon(Icons.more_vert,
                               color: Colors.orange, size: 30),
                           const SizedBox(width: 20),
@@ -118,11 +127,15 @@ class _CalculatorScreenState extends State<CalculatorScreen> {
                         children: [
                           Padding(
                             padding: const EdgeInsets.all(20),
-                            child: Text(equation,
-                                style: const TextStyle(
-                                  fontSize: 40,
-                                  color: Colors.white38,
-                                )),
+                            child: ValueListenableBuilder(
+                                valueListenable: equationNotifier,
+                                builder: (_, equation, ___) {
+                                  return Text(equation,
+                                      style: const TextStyle(
+                                        fontSize: 40,
+                                        color: Colors.white38,
+                                      ));
+                                }),
                           ),
                           IconButton(
                             icon: const Icon(Icons.backspace_outlined,
